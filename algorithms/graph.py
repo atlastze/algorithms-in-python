6879da2a58856e8d6b0f15dd0f022c51ab6adcb5
bruinspaw <bruinspaw@gmail.com>
2018-04-17 21:17:02 +0800

    Refine graph representation

--- algorithms/graph.py
+++ algorithms/graph.py
@@ -23,24 +23,27 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class Graph:
-    """Representation of a simple graph using an adjacency map and an edge map.
-    vertices = { vertex: adjacency deges }
-    edges = { (start_vertex, end_vertex): edges }
+    """Representation of a simple graph using an adjacency map:
+        graph = { u: { v: edge for edge(u, v) in graph.edges}
+                  for u in graph.vertices }                    (map of maps)
     """
 
     # ------------------------- nested Vertex class -------------------------
     class Vertex:
         """Lightweight vertex structure for a graph."""
 
-        def __init__(self, vattr):
-            """Do not call constructor directly. Use Graph's insert_vertex(x)."""
-            self._vattr = vattr
+        def __init__(self, attribute):
+            """Do not call constructor directly.
+            Use Graph's insert_vertex(x).
+
+            """
+            self._attribute = attribute
 
         def __hash__(self):  # will allow vertex to be a map/set key
             return hash(id(self))
 
         def __str__(self):
-            return str(self._vattr)
+            return str(self._attribute)
 
         def __repr__(self):
             return '< %s object at %s >' % (self.__class__, hex(id(self)))
@@ -49,13 +52,16 @@ class Graph:
     # ------------------------- nested Edge class -------------------------
     class Edge:
         """Lightweight edge structure for a graph."""
-        __slots__ = '_origin', '_destination'
+        __slots__ = '_origin', '_destination', '_attribute'
 
-        def __init__(self, u, v, eattr):
-            """Do not call constructor directly. Use Graph's insert_edge(u,v,x)."""
+        def __init__(self, u, v, attribute):
+            """Do not call constructor directly.
+            Use Graph's insert_edge(u,v,x).
+
+            """
             self._origin = u
             self._destination = v
-            self._eattr = eattr
+            self._attribute = attribute
 
         def endpoints(self):
             """Return (u,v) tuple for vertices u and v."""
@@ -67,7 +73,7 @@ class Graph:
                 raise TypeError('vertex type expected')
             if v is self._origin:
                 return self._destination
-            elif v is  self._destination:
+            elif v is self._destination:
                 return self._origin
             else:
                 raise ValueError('vertex not incident to the edge')
@@ -76,9 +82,9 @@ class Graph:
             return hash((self._origin, self._destination))
 
         def __str__(self):
-            return '({0} -> {1} : {2})'.format(self._origin,
-                                          self._destination,
-                                          self._eattr)
+            return '({0} -- {1} : {2})'.format(self._origin,
+                                               self._destination,
+                                               self._attribute)
 
         def __repr__(self):
             return '< %s object at %s >' % (self.__class__, hex(id(self)))
@@ -87,97 +93,167 @@ class Graph:
     # ------------------------- Graph methods -------------------------
     def __init__(self, directed = False):
         """Create an empty graph (undirected, by default)."""
-        self._vertices = {}
-        self._edges = {}
-        self._directed = directed
+        self._outgoing = {}
+        # only create second map for directed graph; use alias for undirected
+        self._incoming = {} if directed else self._outgoing
+
+    def __contains__(self, v):
+        """Override 'in'(membership) operator."""
+        if not isinstance(v, self.Vertex):
+            raise TypeError('vertex type expected')
+        if v in self.vertices():
+            return True
+        else:
+            return False
 
     def _validate_vertex(self, v):
         """Verify that v is a Vertex of this graph."""
-        if not isinstance(v, self.Vertex):
-            raise TypeError('vertex type expected')
-        if v not in self.vertices():
+        if v not in self:
             raise ValueError('vertex not in the graph')
-        return True
 
     def is_directed(self):
         """Return True if this is a directed graph; False if undirected."""
-        return self._directed
+        # directed if maps are distinct
+        return self._incoming is not self._outgoing
 
     def vertex_count(self):
         """Return the number of vertices in the graph."""
-        return len(self._vertices)
+        return len(self._outgoing)
 
     def vertices(self):
         """Return an iteration of all vertices of the graph."""
-        return self._vertices.keys()
+        return self._outgoing.keys()
 
     def edge_count(self):
         """Return the number of edges in the graph."""
-        total = len(self._edges)
+        total = sum(len(self._outgoing[v]) for v in self._outgoing)
         # for undirected graphs, make sure not to double-count edges
         return total if self.is_directed() else total // 2
 
     def edges(self):
         """Return an iteration of all edges of the graph."""
-        return self._edges.values()
+        result = set()  # avoid double-reporting edges of undirected graph
+        for secondary_map in self._outgoing.values():
+            result.update(secondary_map.values())  # add edges to resulting set
+        return result
 
     def edge(self, u, v):
         """Return the edge from u to v, or None if not adjacent."""
         self._validate_vertex(u)
         self._validate_vertex(v)
-        if (u, v) in self._edges.keys():
-                return self._edges[(u, v)]
-        elif not self._directed and (v, u) in self._edges.keys():
-            return self._edges[(v, u)]
+        if v in self._outgoing[u].keys():
+            return self._outgoing[u][v]
         else:
             return None
 
-    def degree(self, v):
+    def out_degree(self, v):
         """Return number of (outgoing) edges incident to vertex v in the graph."""
         self._validate_vertex(v)
-        return len(self._vertices[v])
+        return len(self._outgoing[v])
 
-    def incident_edges(self, v, outgoing=True):
+    def in_degree(self, v):
+        """Return number of (incoming) edges incident to vertex v in the graph."""
+        self._validate_vertex(v)
+        return len(self._incoming[v])
+
+    def outgoing_edges(self, v):
         """Return all (outgoing) edges incident to vertex v in the graph."""
         self._validate_vertex(v)
-        return iter(self._vertices[v].values())
+        return self._outgoing[v].values()
 
-    def insert_vertex(self, vattr):
+    def incoming_edges(self, v):
+        """Return all (incoming) edges incident to vertex v in the graph."""
+        self._validate_vertex(v)
+        return self._incoming[v].values()
+
+    def insert_vertex(self, attribute):
         """Insert and return a new vertex"""
-        v = self.Vertex(vattr)
-        self._vertices[v] = {}
+        v = self.Vertex(attribute)
+        self._outgoing[v] = {}
+        if self.is_directed():
+            self._incoming[v] = {}  # need distinct map for incoming edges
         return v
 
-    def insert_edge(self, u, v, eattr):
+    def remove_vertex(self, v):
+        """Remove a vertex."""
+        self._validate_vertex(v)
+        self._incoming.pop(v)
+        self._outgoing.pop(v)
+        for u in self._outgoing:
+            if v in self._outgoing[u].keys():
+                self._outgoing[u].pop(v)
+
+    def update_vertex(self, v, attribute):
+        """Update a vertex."""
+        self._validate_vertex(v)
+        v._attr = attribute
+
+    def insert_edge(self, u, v, attribute):
         """Insert and return a new Edge from u to v with auxiliary element x.
         Raise a ValueError if u and v are not vertices of the graph.
 
         """
-        if self.edge(u, v) is not None:
-            self._edges[(u, v)]._eattr = eattr
-            return self._edges[(u, v)]
-        e = self.Edge(u, v, eattr)
-        self._vertices[u][v] = e
-        self._edges[(u, v)] = e
+        if self.edge(u, v):
+            return self.update_edge(u, v, attribute)
+        e = self.Edge(u, v, attribute)
+        self._outgoing[u][v] = e
+        self._incoming[v][u] = e    # if undirected, _incoming is _outgoing
+        return e
+
+    def remove_edge(self, u, v):
+        """Remove and return an edge."""
+        e = self.edge(u, v)
+        if e:
+            self._outgoing[u].pop(v)
+            self._incoming[v].pop(u)
+        return e
+
+    def update_edge(self, u, v, attribute):
+        """Update and return an edge."""
+        e = self.edge(u, v)
+        if e:
+            e._attribute = attribute
         return e
 
 
+class Digraph(Graph):
+    """Class Digraph, derived from class Graph."""
+    def __init__(self):
+        Graph.__init__(self, True)
+
+
 if __name__ == '__main__':
-    graph = Graph()
-    a = graph.insert_vertex('A')
-    b = graph.insert_vertex('B')
-    edge = graph.insert_edge(a, b, 'AB')
-    print(edge)
-    print(repr(edge))
-    print(graph.edge(a, b))
+    dg = Digraph()
+    a = dg.insert_vertex('A')
+    b = dg.insert_vertex('B')
+    c = dg.insert_vertex('C')
+    d = dg.insert_vertex('D')
+    ab = dg.insert_edge(a, b, 'AB')
+    ba = dg.insert_edge(b, a, 'BA')
+    ac = dg.insert_edge(a, c, 'AC')
+    ca = dg.insert_edge(c, a, 'CA')
+    ad = dg.insert_edge(a, d, 'AD')
+    bc = dg.insert_edge(b, c, 'BC')
+    bd = dg.insert_edge(b, d, 'BD')
+    da = dg.insert_edge(d, a, 'DA')
+    db = dg.insert_edge(d, b, 'DB')
 
-    print('edges: ')
-    for edge in graph.edges():
-        print(edge)
-    print('vertices: ')
-    for v in graph.vertices():
+    print('>> the original digraph:')
+    for v in dg.vertices():
         print(v)
+    for e in dg.edges():
+        print(e)
 
-    edge = graph.insert_edge(a, b, 'CD')
-    print(edge)
-    print(repr(edge))
+    print('>> remove edge (d->a):')
+    dg.remove_edge(d, a)
+    for v in dg.vertices():
+        print(v)
+    for e in dg.edges():
+        print(e)
+
+    print('>> remove vertex (a):')
+    dg.remove_vertex(a)
+    for v in dg.vertices():
+        print(v)
+    for e in dg.edges():
+        print(e)
